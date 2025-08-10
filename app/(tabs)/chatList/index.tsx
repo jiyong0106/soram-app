84370c2920740f421dc2cc0963de63f8a73634f9
jiyong <parkjiy96@naver.com>
2025-08-10 15:56:48 +0900

    tabs레이아웃 생성

---
+++ app/(tabs)/chatList/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ChatListPage = () => {
+  return (
+    <View>
+      <Text>ChatListPage</Text>
+    </View>
+  );
+};
+
+export default ChatListPage;
+
+const styles = StyleSheet.create({});
