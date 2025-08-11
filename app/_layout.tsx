70ab6cd6e266950a0c8b985d132dc91e17604515
jiyong <parkjiy96@naver.com>
2025-08-11 14:05:56 +0900

    키보드프로바이더 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,20 +2,23 @@ import QueryProvider from "@/libs/QueryProvider";
 import { Stack } from "expo-router";
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { KeyboardProvider } from "react-native-keyboard-controller";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView>
       <QueryProvider>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="index" />
-          <Stack.Screen name="(signUp)" />
-          <Stack.Screen name="(tabs)" />
-        </Stack>
+        <KeyboardProvider>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(signUp)" />
+            <Stack.Screen name="(tabs)" />
+          </Stack>
+        </KeyboardProvider>
       </QueryProvider>
     </GestureHandlerRootView>
   );
