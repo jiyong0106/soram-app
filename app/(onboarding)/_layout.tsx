3079c13235d695a4e3b5d995192a72e0b9debbed
jiyong <parkjiy96@naver.com>
2025-08-14 17:45:17 +0900

    안쓰는 코드 주석처리 2

--- app/(onboarding)/_layout.tsx
+++ app/(onboarding)/_layout.tsx
@@ -1,14 +1,9 @@
-import { Stack, usePathname } from "expo-router";
-import SignupHeader from "@/components/signup/SignupHeader";
+import { Stack } from "expo-router";
 import PageContainer from "@/components/common/PageContainer";
 
 const OnboardingLayout = () => {
-  const pathname = usePathname();
-  // const isRoot = pathname === "/(signup)";
-
   return (
     <PageContainer edges={["top", "bottom"]} padded={false}>
-      {/* <SignupHeader showBack={!isRoot} /> */}
       <Stack
         screenOptions={{
           headerShown: false,
@@ -18,7 +13,7 @@ const OnboardingLayout = () => {
         <Stack.Screen name="index" />
         <Stack.Screen name="GenderPage" />
         <Stack.Screen name="BirthdatePage" />
-        <Stack.Screen name="LocationPage" />
+        {/* <Stack.Screen name="LocationPage" /> */}
         {/* <Stack.Screen name="InterestsPage" /> */}
         {/* <Stack.Screen name="PersonalityPage" /> */}
       </Stack>
