6f9673c241aa022bbce38c5cd4e3af22a8b15c23
jiyong <parkjiy96@naver.com>
2025-08-21 19:46:29 +0900

    index페이지 수정

--- app/(tabs)/profile/_layout.tsx
+++ app/(tabs)/profile/_layout.tsx
@@ -1,15 +1,17 @@
+import PageContainer from "@/components/common/PageContainer";
 import { Stack } from "expo-router";
 
 const ProfileLayout = () => {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: "#fff" },
-      }}
-    >
-      <Stack.Screen name="index" />
-    </Stack>
+    <PageContainer edges={["top"]} padded={false}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="index" />
+      </Stack>
+    </PageContainer>
   );
 };
 export default ProfileLayout;
