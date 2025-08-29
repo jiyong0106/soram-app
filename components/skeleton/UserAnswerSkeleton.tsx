1a9ef1eaef874d4134ddaf2cb23124bc70d017df
jiyong <parkjiy96@naver.com>
2025-08-29 17:24:27 +0900

    답변페이지 리트라이 애니메이션 설정

---
+++ components/skeleton/UserAnswerSkeleton.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const UserAnswerSkeleton = () => {
+  return (
+    <View>
+      <Text>UserAnswerSkeleton</Text>
+    </View>
+  );
+};
+
+export default UserAnswerSkeleton;
+
+const styles = StyleSheet.create({});
