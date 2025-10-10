6c4202ab0b1570fac8672e4a1b7dec03d0abdf86
jiyong <parkjiy96@naver.com>
2025-10-10 15:07:35 +0900

    index페이지 개인정보 처리방침 url연결

--- components/index/TermsNotice.tsx
+++ components/index/TermsNotice.tsx
@@ -1,13 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import AppText from "../common/AppText";
+import * as Linking from "expo-linking";
 
 const TermsNotice = () => {
   return (
     <View style={styles.container}>
-      <AppText style={styles.notice}>
-        가입 시 <AppText style={styles.link}>이용약관</AppText> 및{" "}
-        <AppText style={styles.link}>개인정보 취급방침</AppText>에 동의하게
-        됩니다.
+      <AppText style={styles.notice} numberOfLines={1} ellipsizeMode="tail">
+        가입 시{` `}
+        <AppText
+          style={styles.link}
+          onPress={() =>
+            Linking.openURL(`${process.env.EXPO_PUBLIC_WEB_URL}/terms`)
+          }
+        >
+          이용약관
+        </AppText>
+        {` `}및{` `}
+        <AppText
+          style={styles.link}
+          onPress={() =>
+            Linking.openURL(`${process.env.EXPO_PUBLIC_WEB_URL}/privacy`)
+          }
+        >
+          개인정보 처리방침
+        </AppText>
+        {` `}에 동의하게 됩니다.
       </AppText>
     </View>
   );
